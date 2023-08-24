1bbaa3d9ffc7c61dca048ea0ce2085072d06d3e9
Ergashev Bekmirzo <142732616+ErgashevBekmirzo@users.noreply.github.com>
2023-08-24 17:36:35 +0500

    JavaScript_3-dars

---
+++ mainjs.js
@@ -0,0 +1,36 @@
+/*Math in JS*/
+/*let round = Math.round(8,5);
+let pow = Math.pow(4,3);
+let sqrt = Math.sqrt(81);
+let abs = Math.abs(-12.8)
+let ceil = Math.ceil(49.1)
+let floor = Math.floor(49.9)
+let trunc = Math.trunc(49.9)
+let min = */
+
+
+
+document.write(floor);
+
+
+/*DOM and styles in JS*/
+
+/*let byTag = document.getElementsByTagName("h1");*/
+/*let byId = document.getElementById("idNomi");*/
+/*let byClass = document.getElementsByClassName("classNomi");*/
+/*let bySelector = document.querySelector("classNomi");*/
+/*let bySelectorAll = document.querySelector("h1");*/
+ /*byAll = document.getElementsByTagName("h1").namedItem("idNomi")*/
+
+ /*byAll.style.color = "yellow";
+ byAll.style.backgroundColor = "blue";
+ byAll.style.width = "300px";
+ byAll.style.height = "250px";
+ byAll.style.fontSize = "45px"
+ byAll.style.paddingTop = "90px"
+ byAll.style.boxSizing = "border-box"
+ byAll.style.textAlign = "center"
+ byAll.style.border = "10px solid red"
+ */
+
+console.log(byAll);
